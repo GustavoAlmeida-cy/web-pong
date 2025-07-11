94693f468c0126a597adeddc08da362538a7ad0e
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2025-07-11 20:09:57 -0300

    ball bouncing

---
+++ scripts/ball.js
@@ -0,0 +1,75 @@
+const INITIAL_VELOCITY = 0.025;
+const VELOCITY_INCREASE = 0.000001;
+
+export default class Ball {
+  constructor(ballElement) {
+    this.ballElement = ballElement;
+    this.reset();
+  }
+
+  get x() {
+    return parseFloat(
+      getComputedStyle(this.ballElement).getPropertyValue("--x")
+    );
+  }
+
+  set x(value) {
+    this.ballElement.style.setProperty("--x", value);
+  }
+
+  get y() {
+    return parseFloat(
+      getComputedStyle(this.ballElement).getPropertyValue("--y")
+    );
+  }
+
+  set y(value) {
+    this.ballElement.style.setProperty("--y", value);
+  }
+
+  rect() {
+    return this.ballElement.getBoundingClientRect();
+  }
+
+  reset() {
+    this.x = 50;
+    this.y = 50;
+    this.direction = { x: 0, y: 0 };
+
+    while (
+      Math.abs(this.direction.x) <= 0.2 ||
+      Math.abs(this.direction.x) >= 0.9
+    ) {
+      const heading = randomNumberBetween(0, 2 * Math.PI);
+      this.direction = {
+        x: Math.cos(heading),
+        y: Math.sin(heading),
+      };
+    }
+
+    this.velocity = INITIAL_VELOCITY;
+  }
+
+  update(delta, gameFieldElement) {
+    this.x += this.direction.x * this.velocity * delta;
+    this.y += this.direction.y * this.velocity * delta;
+    this.velocity += VELOCITY_INCREASE * delta;
+
+    const ballRect = this.rect();
+    const fieldRect = gameFieldElement.getBoundingClientRect();
+
+    // Colisão com topo e base do campo
+    if (ballRect.top <= fieldRect.top || ballRect.bottom >= fieldRect.bottom) {
+      this.direction.y *= -1;
+    }
+
+    // Colisão com laterais do campo
+    if (ballRect.left <= fieldRect.left || ballRect.right >= fieldRect.right) {
+      this.direction.x *= -1;
+    }
+  }
+}
+
+function randomNumberBetween(min, max) {
+  return Math.random() * (max - min) + min;
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,24 @@
-console.log("Pong!");
+import Ball from "./ball.js";
+
+// Seleciona os elementos do DOM
+const ballElement = document.getElementById("ball");
+const gameField = document.getElementById("game-field");
+
+// Cria a instância da bola
+const ball = new Ball(ballElement);
+
+let lastTime = null;
+
+// Função principal de atualização do jogo
+function update(time) {
+  if (lastTime != null) {
+    const delta = time - lastTime;
+    ball.update(delta, gameField); // Passa o campo como limite
+  }
+
+  lastTime = time;
+  window.requestAnimationFrame(update);
+}
+
+// Inicia o loop de animação
+window.requestAnimationFrame(update);
